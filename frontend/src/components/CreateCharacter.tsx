d70af9b64d20eeb83a31fe2445cba060230f35dd
Jimmy Cleveland <jimmy.cleveland@clearlink.com>
2019-07-13 17:49:54 -0600

    Frontend: Add logic for createCharacter with name

--- frontend/src/components/CreateCharacter.tsx
+++ frontend/src/components/CreateCharacter.tsx
@@ -1,5 +1,37 @@
-import React from 'react'
+import { gql } from 'apollo-boost'
+import React, { useState } from 'react'
+import { Mutation } from 'react-apollo'
 
-const CreateCharacter = () => <h1>Create Character</h1>
+const CreateCharacter = () => {
+  const [name, setName] = useState('')
+
+  return (
+    <div>
+      <label htmlFor="name">
+        <span>Name:</span>
+        <input
+          id="name"
+          type="text"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setName(event.target.value)
+          }
+        />
+      </label>
+      <Mutation mutation={CREATE_CHARACTER} variables={{ name }}>
+        {(createCharacter: () => void) => (
+          <button onClick={() => createCharacter()}>Submit</button>
+        )}
+      </Mutation>
+    </div>
+  )
+}
+
+const CREATE_CHARACTER = gql`
+  mutation CreateCharacter($name: String!) {
+    createCharacter(name: $name) {
+      name
+    }
+  }
+`
 
 export default CreateCharacter
