b8e0f341fd49605a50bf13b0f5e402690abaef96
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-07-28 15:46:08 -0600

    Add fade in to togglebutton

--- frontend/src/components/ToggleButton.tsx
+++ frontend/src/components/ToggleButton.tsx
@@ -8,13 +8,14 @@ const ToggleButton = ({ isActive, handleClick, children }: IProps) => (
 )
 
 const StyledToggleButton = styled.button`
-  padding: 10px 15px;
+  padding: 10px;
   text-transform: uppercase;
   font-size: 18px;
   font-weight: 500;
   color: #fff;
   border: ${({ theme }) => `solid 1px ${theme.colors.outline}`};
-  background: none;
+  background-color: transparent;
+  transition: background-color 0.3s, border-color 0.3s;
 
   &.active {
     background-color: ${({ theme }) => theme.colors.primaryTransparent};
