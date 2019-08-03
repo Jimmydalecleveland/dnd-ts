0d4c4d910ebe79c99d78cde4dda165a8f7e0c181
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-08-03 12:36:12 -0600

    Abstract GlobalStyle to it's own file

---
+++ frontend/src/components/GlobalStyle.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components'
+import darkTheme from '../theme'
+
+export default createGlobalStyle<{ theme: typeof darkTheme }>`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Rokkitt', serif;
+    color: #fff;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  button {
+    font-family: 'Barlow', sans-serif;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin-top: 0;
+  }
+
+  h3 {
+    margin-bottom: 6px;
+    text-transform: capitalize;
+    font-size: 16px;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.outline};
+  }
+`
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
-import { createGlobalStyle, ThemeProvider, withTheme } from 'styled-components'
+import { ThemeProvider, withTheme } from 'styled-components'
 import 'typeface-barlow'
 import 'typeface-rokkitt'
 import darkTheme from './theme'
@@ -11,55 +11,11 @@ import darkTheme from './theme'
 import Character from './components/Character'
 import Characters from './components/Characters'
 import CreateCharacter from './components/CreateCharacter'
+import GlobalStyle from './components/GlobalStyle'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
 })
-
-const GlobalStyle = createGlobalStyle<{ theme: typeof darkTheme }>`
-  html {
-    box-sizing: border-box;
-  }
-
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
-
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: 'Rokkitt', serif;
-    color: #fff;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  button {
-    font-family: 'Barlow', sans-serif;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  p {
-    margin-top: 0;
-  }
-
-  h3 {
-    margin-bottom: 6px;
-    text-transform: capitalize;
-    font-size: 16px;
-    font-weight: 500;
-    color: ${({ theme }) => theme.colors.outline};
-  }
-`
 
 ReactDOM.render(
   <ApolloProvider client={client}>
