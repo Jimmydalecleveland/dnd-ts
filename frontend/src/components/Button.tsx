09edcfe04b43da7ff087de40fce2d7e19aca17f7
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-08-03 21:42:02 -0600

    Add standard Button

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Button: React.FC<IProps> = ({
+  disabled = false,
+  handleClick,
+  children,
+}) => (
+  <StyledButton disabled={disabled} onClick={handleClick}>
+    {children}
+  </StyledButton>
+)
+
+const StyledButton = styled.button`
+  color: ${({ theme, disabled }) => (disabled ? theme.colors.outline : '#fff')};
+  background-color: ${({ theme }) => theme.colors.secondaryTransparent};
+  border: none;
+  padding: 10px;
+  text-transform: uppercase;
+  font-size: 18px;
+  font-weight: 500;
+  transition: background-color 0.3s, border-color 0.3s;
+`
+
+interface IProps {
+  children: string
+  disabled?: boolean
+  handleClick(): void
+}
+
+export default Button
--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -4,6 +4,7 @@ export default {
     primary: '#45e0c1',
     primaryTransparent: 'rgba(69, 224, 193, 0.6)',
     secondary: '#6fb8e2',
+    secondaryTransparent: 'rgb(111, 184, 226, 0.6)',
     highlight: '#ecb802',
     outline: '#737477',
     negative: '#ed4d4d',
