3a3ce1cbf6e08af2d5597e8b0e015c8cd200fbfc
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-08-03 21:41:51 -0600

    Add ToggleButton disabled state

--- frontend/src/components/ToggleButton.tsx
+++ frontend/src/components/ToggleButton.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const ToggleButton = ({ isActive, handleClick, children }: IProps) => (
-  <StyledToggleButton className={isActive && 'active'} onClick={handleClick}>
+const ToggleButton: React.FC<IProps> = ({
+  disabled = false,
+  isActive,
+  handleClick,
+  children,
+}) => (
+  <StyledToggleButton
+    disabled={disabled}
+    className={isActive && 'active'}
+    onClick={handleClick}
+  >
     {children}
   </StyledToggleButton>
 )
 
 const StyledToggleButton = styled.button`
+  color: ${({ theme, disabled }) => (disabled ? theme.colors.outline : '#fff')};
+  border: ${({ theme }) => `solid 1px ${theme.colors.outline}`};
   padding: 10px;
   text-transform: uppercase;
   font-size: 18px;
   font-weight: 500;
-  color: #fff;
-  border: ${({ theme }) => `solid 1px ${theme.colors.outline}`};
   background-color: transparent;
   transition: background-color 0.3s, border-color 0.3s;
 
@@ -24,8 +33,9 @@ const StyledToggleButton = styled.button`
 `
 
 interface IProps {
-  isActive: boolean
   children: string
+  disabled?: boolean
+  isActive: boolean
   handleClick(): void
 }
 
