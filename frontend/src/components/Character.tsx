6b9f059b5da5f476a310d2740e9bc1643add1b7c
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-08-18 18:13:23 -0600

    Refactor Character to apollo hooks

--- frontend/src/components/Character.tsx
+++ frontend/src/components/Character.tsx
@@ -1,44 +1,40 @@
 import { gql } from 'apollo-boost'
 import React from 'react'
-import { Mutation, Query } from 'react-apollo'
+import { useMutation, useQuery } from 'react-apollo'
 import { RouteComponentProps } from 'react-router-dom'
 
 const Character = ({ match, history }: RouteComponentProps<IProps>) => {
   const { id: characterID } = match.params
-  return (
-    <Query<IData, IVariables>
-      query={CHARACTER_QUERY}
-      variables={{ ID: characterID }}
-    >
-      {({ loading, error, data }) => {
-        if (loading) {
-          return <p>loading...</p>
-        }
-        if (error) {
-          return <p>Error: {error}</p>
-        }
+  const { loading, data } = useQuery<IQueryData, IQueryVariables>(
+    CHARACTER_QUERY,
+    {
+      variables: {
+        ID: characterID,
+      },
+    }
+  )
+  const [deleteCharacter] = useMutation<{}, IMutationVariables>(
+    DELETE_CHARACTER,
+    {
+      onCompleted: () => history.push('/characters'),
+      variables: { ID: characterID },
+    }
+  )
 
-        return (
-          <div>
-            <h1>{data.character.name}</h1>
-            <h2>
-              {data.character.subrace
-                ? data.character.subrace.name
-                : data.character.race.name}
-            </h2>
-            <Mutation
-              mutation={DELETE_CHARACTER}
-              variables={{ ID: data.character.ID }}
-              onCompleted={() => history.push('/characters')}
-            >
-              {(deleteCharacter: () => void) => (
-                <button onClick={() => deleteCharacter()}>Delete</button>
-              )}
-            </Mutation>
-          </div>
-        )
-      }}
-    </Query>
+  if (loading) {
+    return <p>loading...</p>
+  }
+
+  return (
+    <div>
+      <h1>{data.character.name}</h1>
+      <h2>
+        {data.character.subrace
+          ? data.character.subrace.name
+          : data.character.race.name}
+      </h2>
+      <button onClick={() => deleteCharacter()}>Delete</button>
+    </div>
   )
 }
 
@@ -69,7 +65,7 @@ interface IProps {
   id: string
 }
 
-interface IData {
+interface IQueryData {
   character: {
     ID: string
     name: string
@@ -83,7 +79,11 @@ interface IRace {
   name: string
 }
 
-interface IVariables {
+interface IQueryVariables {
+  ID: string
+}
+
+interface IMutationVariables {
   ID: string
 }
 
